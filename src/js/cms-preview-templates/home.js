cf651fdce654d0186a089e57869b3953158674fe
John Lian <jlian@users.noreply.github.com>
2023-01-18 18:10:56 -0800

    Fix typo

--- src/js/cms-preview-templates/home.js
+++ src/js/cms-preview-templates/home.js
@@ -5,7 +5,7 @@ import Jumbotron from "./components/jumbotron";
 
 export default class PostPreview extends React.Component {
   render() {
-    const {entry, getAsset} = this.props;
+    const { entry, getAsset, widgetFor } = this.props;
     let image = getAsset(entry.getIn(["data", "image"]));
 
     // Bit of a nasty hack to make relative paths work as expected as a background image here
